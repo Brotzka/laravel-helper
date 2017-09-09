d9e9ab77ef664f30e8a59f79240a09181a893f57
vagrant <vagrant@homestead>
2017-09-09 05:00:16 +0000

    Test 1

---
+++ src/HelperServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Brotzka\LaravelHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
